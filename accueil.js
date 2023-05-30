9177034a1a375ca76888f8154ea5a2071d46d000
Poteau Florian <florianpoteau59@gmail.com>
2023-05-30 14:00:20 +0200

    test gitignore

---
+++ accueil.js
@@ -0,0 +1,34 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Fetch
+    
+    
+    function fetch(url, method, fun) {
+        //Initialisation de XHR
+        const request = new XMLHttpRequest();
+        request.addEventListener("load", fun);
+        //Spécifier le type d'appel [ GET, POST, PUT, PATCH, DELETE ] et l'URL
+        request.open(method, url);
+        //Spécification que je veux du JSON en type de retour
+        request.setRequestHeader('Accept', "application/json");
+        //Permet d'envoyer la requête
+        request.send();
+      }
+
+
+
+
+    
+
+    fetch(`https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=`, "GET", championListe)
+
+    function championListe(){
+        const request = JSON.parse(this.response);
+
+        console.log(request);
+    }
+
+})
