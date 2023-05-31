ebcd04084decf3c86c12ba36319ad6467f4e77fe
Poteau Florian <florianpoteau59@gmail.com>
2023-05-31 11:16:51 +0200

    add image + graphique

--- accueil.js
+++ accueil.js
@@ -18,18 +18,85 @@ document.addEventListener('DOMContentLoaded', function () {
         request.send();
       }
       
+    //   Variable pour récupérer toutes les images
+const listeImage = document.querySelectorAll(".containerImg");
 
-
-
+// Variable pour récupérer tout les container qui englobe les images et les graphiques
+const container = document.querySelectorAll(".container")
 
     
 
-    fetch(`https://br1.api.riotgames.com/lol/platform/v3/champion-rotation?api_key=${api_key}`, "GET", championListe)
+    fetch(`http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion.json`, "GET", championListe)
 
     function championListe(){
         const request = JSON.parse(this.response);
 
-        console.log(request);
+        const champions = request.data;
+
+        // Boucle pour afficher que 9 éléments
+
+        for (let i=0; i<=9; i++){
+
+            // Récupération des champions par champion
+        const championKey = Object.keys(champions)[i+1];
+
+        const champion = champions[championKey];
+
+        // Récupération de l'image de chaque champion
+        const championImage = champion.image.full;
+
+        // Création de l'image dans le dom
+        const imageChampion = document.createElement("img")
+
+        imageChampion.src = `http://ddragon.leagueoflegends.com/cdn/13.10.1/img/champion/${championImage}`;
+
+        // Puisque listeImage est une nodeList, création d'une variable qui permet d'itérer sur cette liste afin de récupérer élément par élément
+        const containerImg = listeImage[i];
+    containerImg.appendChild(imageChampion);
+
+    // Evenement au click des images
+
+            imageChampion.addEventListener("click", () =>{
+
+
+                // Création du graphique en html
+                const graphique = document.createElement("div")
+                const canva = document.createElement("canvas")
+                canva.id = "myChart"
+
+                graphique.appendChild(canva);
+
+                // Puisque container est une nodeList, création d'une variable qui permet d'itérer sur cette liste afin de récupérer élément par élément
+                const detailGraphique = container[i]
+                detailGraphique.appendChild(graphique);
+
+                // Création du graphique par chart.js
+                const ctx = document.getElementById('myChart');
+
+                    new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            borderWidth: 1
+                        }]
+                        },
+                        options: {
+                        scales: {
+                            y: {
+                            beginAtZero: true
+                            }
+                        }
+                        }
+                    });
+
+                    
+                
+            })
+
+          }
     }
 
 })
